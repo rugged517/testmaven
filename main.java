4049649ffd79cbae670ce2f888829447c16f935a
Simon Lienhard <simon.lienhard@hs-furtwangen.de>
2015-11-24 16:08:15 +0100

    second

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class main{
+   public static void main(String args[]){
+    System.out.println("Gib was ein: ");
+    Scanner sc = new Scanner(System.in);
+	    
+String str = sc.nextLine(); 
+    System.out.println(""+str.toUpperCase());
+   }
+}
---
+++ src/main/java/de/simon/App.java
@@ -0,0 +1,13 @@
+package de.simon;
+import java.io.*;
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       	Scanner scanner = new Scanner(System.in);
+	String str = scanner.nextLine();
+	System.out.println(""+str.toUpperCase());
+    }
+}
